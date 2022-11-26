5865279b2f08cfe99f3be17d60d23ab98b35d8ba
GOTWIC <swagnik17@gmail.com>
2022-11-25 19:32:28 -0500

    Added a game lobby/room. Transitioned from NetworkManager to NetworkRoomManager

--- Assets/SCRIPTS/MyNetworkManager.cs
+++ Assets/SCRIPTS/MyNetworkManager.cs
@@ -2,21 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
+using UnityEditor.MemoryProfiler;
+using System;
 
-public class MyNetworkManager : NetworkManager
+public class MyNetworkManager : NetworkRoomManager
 {
 
     [SerializeField] private GameObject standard = null;
-    public override void OnServerAddPlayer(NetworkConnectionToClient conn)
-    {
-        base.OnServerAddPlayer(conn);
 
+    public override bool OnRoomServerSceneLoadedForPlayer(NetworkConnectionToClient conn, GameObject roomPlayer, GameObject gamePlayer)
+    {
+        spawnRobot(conn, gamePlayer);
+        return base.OnRoomServerSceneLoadedForPlayer(conn, roomPlayer, gamePlayer);
+    }
+
+    private void spawnRobot(NetworkConnectionToClient conn, GameObject spawnPointReference)
+    {
         // Spawns on Server
         // "conn.identity.transform is how you get transform of the player object"
-        GameObject spawnerInstance = Instantiate(standard, conn.identity.transform.position, conn.identity.transform.rotation);
+        GameObject spawnerInstance = Instantiate(standard, spawnPointReference.transform.position, spawnPointReference.transform.rotation);
 
         //Spawns on Network
         NetworkServer.Spawn(spawnerInstance, conn);
-
     }
 }
--- Assets/SCRIPTS/PlayerScript.cs
+++ Assets/SCRIPTS/PlayerScript.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerScript : MonoBehaviour
 {
 
-    bool lockedCursor = false;
-    void Start()
+    void Update()
     {
+        if (SceneManager.GetActiveScene().name == "Room") { return; }
+
+        return; // befcuase cursing locking is annoying af
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
