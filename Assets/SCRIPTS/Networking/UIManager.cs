59830572b20a3c22621ebd819f54aa32eef1d866
GOTWIC <swagnik17@gmail.com>
2022-11-30 11:50:52 -0500

    Disable UI on loading main scene

--- Assets/SCRIPTS/Networking/UIManager.cs
+++ Assets/SCRIPTS/Networking/UIManager.cs
@@ -1,4 +1,6 @@
+using System.Diagnostics.Tracing;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
@@ -6,16 +8,19 @@ public class UIManager : MonoBehaviour
     [SerializeField] private GameObject robotSelection = null;
     [SerializeField] private GameObject hostingJoining = null;
     [SerializeField] private GameObject playerName = null;
+    [SerializeField] private GameObject background = null;
 
 
     private static UIManager uiManagerInstance;
+
+    private Scene sceneOnPrevFrame;
 
 
     private void Awake()
     {
         int numMusicPlayers = FindObjectsOfType<UIManager>().Length;
         if (numMusicPlayers != 1) { Destroy(this.gameObject); }
-        else { DontDestroyOnLoad(gameObject); } 
+        else { DontDestroyOnLoad(gameObject); }
     }
 
     void Start()
@@ -29,12 +34,33 @@ public class UIManager : MonoBehaviour
 
     }
 
+    private void Update()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+
+        if (sceneOnPrevFrame == null) { sceneOnPrevFrame = scene; }
+
+        if(scene.name == "Main")
+        {
+            goToMain();
+        }
+
+        // Check if scene changed, can change this to an event system later, linked to the room controls in the network manager
+        if(scene.name == "Room" && sceneOnPrevFrame.name == "Main")
+        {
+            goToRoom();
+        }
+
+        sceneOnPrevFrame = scene;
+    }
+
     public void goToRoom()
     {
         hostingJoining.SetActive(false);
         playerName.SetActive(false);
         teamSelection.SetActive(true);
         robotSelection.SetActive(true);
+        background.SetActive(true);
     }
 
     public void goToHome()
@@ -43,5 +69,15 @@ public class UIManager : MonoBehaviour
         playerName.SetActive(true);
         teamSelection.SetActive(false);
         robotSelection.SetActive(false);
+        background.SetActive(true);
+    }
+
+    public void goToMain()
+    {
+        hostingJoining.SetActive(false);
+        playerName.SetActive(false);
+        teamSelection.SetActive(false);
+        robotSelection.SetActive(false);
+        background.SetActive(false);
     }
 }
