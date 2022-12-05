53fe6228bb12a9176b843199b4c5aa84ff666a8c
GOTWIC <swagnik17@gmail.com>
2022-12-05 16:19:09 -0500

    Added Ultra Violet Logo to Menu Screen

--- Assets/Mirror/Components/NetworkRoomManager.cs
+++ Assets/Mirror/Components/NetworkRoomManager.cs
@@ -697,6 +697,7 @@ namespace Mirror
             if (!showRoomGUI)
                 return;
 
+            
             if (NetworkServer.active && IsSceneActive(GameplayScene))
             {
                 GUILayout.BeginArea(new Rect(Screen.width - 150f, 10f, 140f, 30f));
@@ -705,8 +706,10 @@ namespace Mirror
                 GUILayout.EndArea();
             }
 
+            /*
             if (IsSceneActive(RoomScene))
                 GUI.Box(new Rect(10f, 180f, 520f, 150f), "PLAYERS");
+            */
         }
 
         #endregion
