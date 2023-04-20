a5bcea10f41d12153ed9766b235e344190c5de07
GOTWIC <swagnik17@gmail.com>
2023-04-20 00:50:39 -0400

    Added Team-based robot spawning system
    Robots now spawn depending on which team they are on, rather than just spawning at the next available spawn point

--- Assets/SCRIPTS/Networking/MyNetworkManager.cs
+++ Assets/SCRIPTS/Networking/MyNetworkManager.cs
@@ -12,6 +12,30 @@ public class MyNetworkManager : NetworkRoomManager
     [SerializeField] private GameObject hero = null;
 
 
+    [SerializeField] List<GameObject> redSpawnPoints= new List<GameObject>();
+    [SerializeField] List<GameObject> blueSpawnPoints = new List<GameObject>();
+
+    public int redMemberCount = 0;
+    public int blueMemberCount = 0;
+
+
+    public override void OnServerSceneChanged(string sceneName)
+    {
+        base.OnServerSceneChanged(sceneName);
+        getSpawnPoints();
+    }
+    
+    public void getSpawnPoints()
+    {
+        GameObject[] spawnPoints = GameObject.FindGameObjectsWithTag("Spawn Point");        
+
+        foreach (GameObject spawnpoint in spawnPoints)
+        {
+            if (Int32.Parse(spawnpoint.name.Remove(0, 5)) % 2 == 0) { redSpawnPoints.Add(spawnpoint); }
+            else { blueSpawnPoints.Add(spawnpoint); }
+        }
+    }
+
     public override bool OnRoomServerSceneLoadedForPlayer(NetworkConnectionToClient conn, GameObject roomPlayer, GameObject gamePlayer)
     {
         spawnRobot(conn, roomPlayer, gamePlayer);
@@ -38,6 +62,21 @@ public class MyNetworkManager : NetworkRoomManager
             robot = hero;
         }
 
+
+        if (teamSelection == 0)
+        {
+            gamePlayer.transform.position = redSpawnPoints[redMemberCount].transform.position;
+            gamePlayer.transform.rotation = redSpawnPoints[redMemberCount].transform.rotation;
+            redMemberCount++;
+        }
+
+        else if (teamSelection == 1)
+        {
+            gamePlayer.transform.position = blueSpawnPoints[blueMemberCount].transform.position;
+            gamePlayer.transform.rotation = blueSpawnPoints[blueMemberCount].transform.rotation;
+            blueMemberCount++;
+        }
+        
         // Spawns on Server
         // "conn.identity.transform" is how you get transform of the player object
         GameObject spawnerInstance = Instantiate(robot, gamePlayer.transform.position, gamePlayer.transform.rotation);
